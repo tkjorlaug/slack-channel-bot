0535bc833bfbe9d0bc959c0a934f6e03fa8f86de
tkjorlaug <tkjorlaug@q-pac.com>
2026-03-15 20:13:53 -0400

    Update migrate.js

--- db/migrate.js
+++ db/migrate.js
@@ -1,6 +1,11 @@
-const { Pool } = require('pg');
-const fs = require('fs');
-const path = require('path');
+import pg from 'pg';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const { Pool } = pg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
